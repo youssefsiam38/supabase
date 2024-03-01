cbd6d52368ff487c3712cd6b963a23e08fc0ea35
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-03-01 17:40:08 +0100

    access clock for animations

--- apps/www/components/LaunchWeek/11/Dot.js
+++ apps/www/components/LaunchWeek/11/Dot.js
@@ -4,12 +4,14 @@ export function Dot(x, y, w, h) {
   this.w = w
   this.h = h
 
-  this.draw = function (c) {
+  this.draw = function (c, clock) {
     c?.fillRect(this.x, this.y, this.w, this.h)
     c?.fill()
+    // this.x += clock / 10000
+    // c?.fillStyle = '#fff'
   }
 
-  this.update = function (c) {
-    this.draw(c)
+  this.update = function (c, clock) {
+    this.draw(c, clock)
   }
 }
--- apps/www/pages/launch-week/11/canvas-v1.tsx
+++ apps/www/pages/launch-week/11/canvas-v1.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import { isBrowser } from 'common'
+import { debounce } from 'lodash'
 import DefaultLayout from '~/components/Layouts/Default'
 import { Dot } from '~/components/LaunchWeek/11/Dot'
-import { isBrowser } from 'common'
+import { useFrame } from '@react-three/fiber'
+import { useAnimationFrame } from 'framer-motion'
 
 const LW11 = () => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null)
   const [size, setSize] = useState({ w: 1200, h: 800 })
 
-  const DOT_AREA = 10
+  const DOT_AREA = 30
   const DOT_SIZE = 1
   let GRID_COLS = Math.floor(canvasRef.current?.getBoundingClientRect().width! / DOT_AREA)
   let GRID_ROWS = Math.floor(canvasRef.current?.getBoundingClientRect().height! / DOT_AREA)
@@ -25,25 +28,31 @@ const LW11 = () => {
     for (let i = 0; i < GRID_COLS; i++) {
       for (let j = 0; j < GRID_ROWS; j++) {
         // if (!canvasRef.current) return
+        // const dotSize = Math.random() > 0.98 ? 2 : DOT_SIZE
+        const dotSize = Math.random() > 0.98 ? 2.5 : Math.random() * DOT_SIZE * 1.5
         const x = (canvasRef.current?.getBoundingClientRect().width! / GRID_COLS) * i
         const y = (canvasRef.current?.getBoundingClientRect().height! / GRID_ROWS) * j
-        const w = DOT_SIZE
-        const h = DOT_SIZE
+        const w = dotSize
+        const h = dotSize
         dotsArray.push(new Dot(x, y, w, h))
       }
     }
 
-    animate()
-    console.log(GRID_COLS, GRID_ROWS, dotsArray)
+    // animate()
   }
 
-  function animate() {
+  useAnimationFrame((clock) => {
+    animate && animate(clock)
+  })
+
+  function animate(clock: number) {
     if (!isBrowser) return
-    window.requestAnimationFrame(animate)
+
+    // window.requestAnimationFrame(animate)
     c?.clearRect(0, 0, size.w, size.h)
 
     for (let i = 0; i < dotsArray.length; i++) {
-      dotsArray[i].update(c)
+      dotsArray[i].update(c, clock)
     }
   }
 
@@ -54,9 +63,10 @@ const LW11 = () => {
 
   useEffect(() => {
     if (!isBrowser) return
-    window.addEventListener('resize', () => resize())
+    const handleDebouncedResize = debounce(() => resize(), 10)
+    window.addEventListener('resize', handleDebouncedResize)
 
-    return () => window.removeEventListener('resize', resize)
+    return () => window.removeEventListener('resize', handleDebouncedResize)
   }, [])
 
   init()
