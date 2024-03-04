4b0b15888e3454a4eecac4b16c9fcfd0dfd7aab7
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2024-03-04 18:41:42 +0100

    add gui to canvas grid

--- apps/www/pages/launch-week/11/canvas-v2.tsx
+++ apps/www/pages/launch-week/11/canvas-v2.tsx
@@ -5,11 +5,22 @@ import { debounce } from 'lodash'
 import DefaultLayout from '~/components/Layouts/Default'
 import { Dot } from '~/components/LaunchWeek/11/Dot2'
 
+const defaultConfig = {
+  dotGrid: 15,
+  randomizeLargeDots: 4,
+  randomizeSmallDots: 0.7,
+  minSpeed: 1,
+  maxSpeed: 4,
+  minOscillation: 1,
+  maxOscillation: 10,
+}
+
 const LW11 = () => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null)
   const [size, setSize] = useState({ w: 1200, h: 800 })
+  const [config, setConfig] = useState(defaultConfig)
 
-  const DOT_AREA = 15
+  const DOT_AREA = config.dotGrid
   let GRID_COLS = Math.floor(canvasRef.current?.getBoundingClientRect().width! / DOT_AREA)
   let GRID_ROWS = Math.floor(canvasRef.current?.getBoundingClientRect().height! / DOT_AREA)
   const c = canvasRef.current?.getContext('2d')
@@ -29,10 +40,14 @@ const LW11 = () => {
         const isLarge = Math.random() > 0.99
         const isAnimated = isLarge || Math.random() > 0.75
         const direction = isAnimated ? (Math.random() > 0.5 ? 'vertical' : 'horizontal') : undefined
-        const speed = isAnimated ? anime.random(1, 4) : undefined
+        const speed = isAnimated ? anime.random(config.minSpeed, config.maxSpeed) : undefined
         const isReverse = isAnimated ? Math.random() > 0.5 : undefined
-        const oscillation = isAnimated ? anime.random(1, 10).toFixed() : undefined
-        const dotSize = isLarge ? Math.random() * 4 : Math.random() * 0.7
+        const oscillation = isAnimated
+          ? anime.random(config.minOscillation, config.maxOscillation).toFixed()
+          : undefined
+        const dotSize = isLarge
+          ? Math.random() * config.randomizeLargeDots
+          : Math.random() * config.randomizeSmallDots
         const endPos = {
           x: anime
             .random(
@@ -76,6 +91,46 @@ const LW11 = () => {
     }
 
     animate(0)
+  }
+
+  const handleSetConfig = (name: string, value: any) => {
+    setConfig((prevConfig: any) => ({ ...prevConfig, [name]: value }))
+  }
+
+  async function initGUI() {
+    // if (!isSandbox) return
+    const dat = await import('dat.gui')
+    const gui = new dat.GUI()
+    gui.width = 500
+
+    gui
+      .add(config, 'dotGrid')
+      .name('Dot area')
+      .onChange((v) => handleSetConfig('dotGrid', v))
+    gui
+      .add(config, 'randomizeLargeDots')
+      .name('Large Dots size')
+      .onChange((v) => handleSetConfig('randomizeLargeDots', v))
+    gui
+      .add(config, 'randomizeSmallDots')
+      .name('Small Dots size')
+      .onChange((v) => handleSetConfig('randomizeSmallDots', v))
+    gui
+      .add(config, 'minSpeed')
+      .name('Min Speed')
+      .onChange((v) => handleSetConfig('minSpeed', v))
+    gui
+      .add(config, 'maxSpeed')
+      .name('Max Speed')
+      .onChange((v) => handleSetConfig('maxSpeed', v))
+    gui
+      .add(config, 'minOscillation')
+      .name('Min Oscillation')
+      .onChange((v) => handleSetConfig('minOscillation', v))
+    gui
+      .add(config, 'maxOscillation')
+      .name('Max Oscillation')
+      .onChange((v) => handleSetConfig('maxOscillation', v))
   }
 
   function animate(clock?: number) {
@@ -139,6 +194,7 @@ const LW11 = () => {
   useEffect(() => {
     resize()
     init()
+    initGUI()
   }, [])
 
   init()
