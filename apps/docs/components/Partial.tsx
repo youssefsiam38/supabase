03051db86e1600f3133c2e5293e0a8b5648a9f9a
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-05-03 22:23:36 -0400

    enhance: add dummy Partial files
    Creating components named Partial will get super confusing because
    Partial tags get swapped out in MDX files. Create dummy files in the
    most likely places someone would create such a component, with a comment
    to warn them.

---
+++ apps/docs/components/Partial.tsx
@@ -0,0 +1,6 @@
+/**
+ * This file exists to discourage you from creating a component named `Partial`.
+ *
+ * It will get super confusing because MDX files swap out <Partial /> tags with
+ * string replacements.
+ */
